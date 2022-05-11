7d1a9de993a6fe4a2d28466c6e951b45c02f36ea
d9m0n <c2h8e9d4@gmail.com>
2022-05-11 23:07:43 +0300

    charts

---
+++ src/Components/Chart/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from 'chart.js';
+import { Line, Pie } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 3),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => 4),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const data2 = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 3),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => 4),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+interface IChartProps {
+  line?: Boolean;
+}
+
+const Chart: React.FC<IChartProps> = ({ line }) => {
+  return (
+    <>{line ? <Line options={options} data={data} /> : <Pie options={options} data={data2} />}</>
+  );
+};
+
+export default Chart;
--- src/Pages/mainPage/index.tsx
+++ src/Pages/mainPage/index.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
-import PageTopBar from 'Components/PageTopBar';
-import PageTable from 'Components/PageTable';
-import Button from 'Components/Button';
+import Chart from 'Components/Chart';
 
 import './index.scss';
 
 const MainPage = () => {
   return (
     <div className="flex w-full flex-col">
-      <PageTopBar>
-        <Button name="Добавить дом" />
-        <Button name="Добавить помещение" />
-      </PageTopBar>
-      <div className="mt-8 ">
-        <PageTable />
+      <div className="flex p-4 justify-center">
+        <div className="px-4 py-8 shadow-md rounded text-center w-80 mx-4">
+          <p className="text-2xl mb-4">Лицевых счетов</p>
+          <b className="text-3xl text-accent">1045</b>
+        </div>
+        <div className="px-4 py-8 shadow-md rounded text-center w-80 mx-4">
+          <p className="text-2xl mb-4">Последнее проведенное голосование</p>
+          <b className="text-3xl text-accent ">23.04.2022</b>
+        </div>
+        <div className="px-4 py-8 shadow-md rounded text-center w-80 mx-4">
+          <p className="text-2xl mb-4">Договоров управления</p>
+          <b className="text-3xl text-accent">29</b>
+        </div>
       </div>
-      <Outlet />
+      <div className="rounded shadow-md p-4">
+        <Chart line={false} />
+      </div>
+      <div className="flex">
+        <Chart line={true} />
+      </div>
     </div>
   );
 };
